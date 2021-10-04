4b284eb5fe183db89e2fd0a3caf9d6b50df2da5d
Gavin Corkery <GavinCorkery@hotmail.com>
2021-10-04 16:41:09 -0400

    Finished 12th tutorial video

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return ( 
+        <footer className = "footer">
+            <h4>Copyright © 2021</h4>
+            <h4>Created by Gavin Corkery</h4>
+        </footer>
+     );
+}
+ 
+export default Footer
